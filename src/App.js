64422a9f47bd290400cad52043e998a435db20b3
Boat <Boat_loading@hotmail.com>
2022-03-27 12:11:54 +0700

    complete

---
+++ src/App.js
@@ -0,0 +1,84 @@
+import React, {useState,useEffect} from 'react';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+import {CssBaseline, Grid} from '@material-ui/core';
+
+import {getPlacesData, getWeatherData} from './api';
+
+
+function App() {
+
+  const [places, setPlaces] = useState([]);
+  const [weatherData, setWeatherData] = useState([]);
+  const [filteredPlaces,setFilteredPlaces] = useState([]);
+  const [childClicked, setchildClicked] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState('restaurants');
+  const [rating,setRating] = useState('');
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
+      setCoordinates({ lat:latitude, lng:longitude})
+    })
+  },[]);
+
+  useEffect(()=> {
+    const filteredPlace = places.filter((place)=> place.rating > rating);
+
+    setFilteredPlaces(filteredPlace);
+  },[rating])
+
+  useEffect(()=>{
+    if(bounds.sw && bounds.ne) {
+    setIsLoading(true);
+
+    getWeatherData(coordinates.lat,coordinates.lng)
+      .then((data) => setWeatherData(data));
+
+    getPlacesData(type,bounds.ne, bounds.sw)
+      .then((data)=>{
+        setPlaces(data?.filter((place)=> place.name && place.num_reviews > 0));
+        setFilteredPlaces([]);
+        setIsLoading(false);
+      })
+    }
+  },[type, bounds]);
+
+
+  return (
+    <>
+      <CssBaseline />
+      <Header setCoordinates={setCoordinates}/>
+      <Grid container spacing={3} style={{ width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List 
+          places = {filteredPlaces.length ? filteredPlaces : places} 
+          childClicked = {childClicked}
+          isLoading={isLoading}
+          type={type}
+          setType={setType}
+          rating={rating}
+          setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map
+            setCoordinates = {setCoordinates} 
+            setBounds = {setBounds}
+            coordinates = {coordinates}
+            places = {filteredPlaces.length ? filteredPlaces : places}
+            setchildClicked = {setchildClicked}
+            weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/List/List.jsx
@@ -0,0 +1,56 @@
+import React, {useState, useEffect, createRef} from 'react';
+import{CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+import useStyles from './styles';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+
+
+
+export default function List({places, childClicked, isLoading, type, setType, rating, setRating}) {
+  const classes = useStyles();
+  const [elRefs, setElRefs] = useState([]);
+  useEffect(()=>{
+    setElRefs((refs) => Array(places?.length).fill().map((_, i) => refs[i] || createRef()));
+  },[places])
+
+
+  return (
+    <div className={classes.container}>
+      <Typography variant='h4'>
+        Restaurants, Hotels & Attractions around you
+      </Typography>
+      {isLoading? (
+        <div className={classes.loading}> 
+          <CircularProgress size="5rem" />
+        </div>
+      ) : (
+      <>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e)=>setType(e.target.value)}>
+          <MenuItem value="restaurants">restaurants</MenuItem>
+          <MenuItem value="hotels">hotels</MenuItem>
+          <MenuItem value="attractions">attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e)=>setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+      <Grid container spacing={3} className={classes.list} >
+      {places?.map((place, i) => (
+              <Grid ref={elRefs[i]} key={i} item xs={12}>
+                <PlaceDetails selected={Number(childClicked) === i} refProp={elRefs[i]} place={place} />
+              </Grid>
+            ))}
+      </Grid>
+      </>
+      )}
+
+    </div>
+  )
+}
